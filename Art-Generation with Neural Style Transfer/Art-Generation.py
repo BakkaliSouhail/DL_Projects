49ba78f70a50651876c935e6cf0cc45f71f07a3d
Sbakkali <souhail.bkl@gmail.com>
2019-06-05 21:48:49 +0200

    no message

--- Art-Generation with Neural Style Transfer/Art-Generation.py
+++ Art-Generation with Neural Style Transfer/Art-Generation.py
@@ -1,18 +1,3 @@
-
-# coding: utf-8
-
-# # Deep Learning & Art: Neural Style Transfer
-# 
-# Welcome to the second assignment of this week. In this assignment, you will learn about Neural Style Transfer. This algorithm was created by Gatys et al. (2015) (https://arxiv.org/abs/1508.06576). 
-# 
-# **In this assignment, you will:**
-# - Implement the neural style transfer algorithm 
-# - Generate novel artistic images using your algorithm 
-# 
-# Most of the algorithms you've studied optimize a cost function to get a set of parameter values. In Neural Style Transfer, you'll optimize a cost function to get pixel values!
-
-# In[1]:
-
 import os
 import sys
 import scipy.io
@@ -27,114 +12,29 @@ import tensorflow as tf
 get_ipython().magic('matplotlib inline')
 
 
-# ## 1 - Problem Statement
-# 
-# Neural Style Transfer (NST) is one of the most fun techniques in deep learning. As seen below, it merges two images, namely, a "content" image (C) and a "style" image (S), to create a "generated" image (G). The generated image G combines the "content" of the image C with the "style" of image S. 
-# 
-# In this example, you are going to generate an image of the Louvre museum in Paris (content image C), mixed with a painting by Claude Monet, a leader of the impressionist movement (style image S).
-# <img src="images/louvre_generated.png" style="width:750px;height:200px;">
-# 
-# Let's see how you can do this. 
-
-# ## 2 - Transfer Learning
-# 
-# Neural Style Transfer (NST) uses a previously trained convolutional network, and builds on top of that. The idea of using a network trained on a different task and applying it to a new task is called transfer learning. 
-# 
-# Following the original NST paper (https://arxiv.org/abs/1508.06576), we will use the VGG network. Specifically, we'll use VGG-19, a 19-layer version of the VGG network. This model has already been trained on the very large ImageNet database, and thus has learned to recognize a variety of low level features (at the earlier layers) and high level features (at the deeper layers). 
-# 
-# Run the following code to load parameters from the VGG model. This may take a few seconds. 
-
-# In[2]:
-
 model = load_vgg_model("pretrained-model/imagenet-vgg-verydeep-19.mat")
 print(model)
 
 
-# The model is stored in a python dictionary where each variable name is the key and the corresponding value is a tensor containing that variable's value. To run an image through this network, you just have to feed the image to the model. In TensorFlow, you can do so using the [tf.assign](https://www.tensorflow.org/api_docs/python/tf/assign) function. In particular, you will use the assign function like this:  
-# ```python
-# model["input"].assign(image)
-# ```
-# This assigns the image as an input to the model. After this, if you want to access the activations of a particular layer, say layer `4_2` when the network is run on this image, you would run a TensorFlow session on the correct tensor `conv4_2`, as follows:  
-# ```python
-# sess.run(model["conv4_2"])
-# ```
-
-# ## 3 - Neural Style Transfer 
-# 
-# We will build the NST algorithm in three steps:
-# 
-# - Build the content cost function $J_{content}(C,G)$
-# - Build the style cost function $J_{style}(S,G)$
-# - Put it together to get $J(G) = \alpha J_{content}(C,G) + \beta J_{style}(S,G)$. 
-# 
-# ### 3.1 - Computing the content cost
-# 
-# In our running example, the content image C will be the picture of the Louvre Museum in Paris. Run the code below to see a picture of the Louvre.
-
-# In[3]:
-
 content_image = scipy.misc.imread("images/louvre.jpg")
 imshow(content_image)
-
-
-# The content image (C) shows the Louvre museum's pyramid surrounded by old Paris buildings, against a sunny sky with a few clouds.
-# 
-# ** 3.1.1 - How do you ensure the generated image G matches the content of the image C?**
-# 
-# As we saw in lecture, the earlier (shallower) layers of a ConvNet tend to detect lower-level features such as edges and simple textures, and the later (deeper) layers tend to detect higher-level features such as more complex textures as well as object classes. 
-# 
-# We would like the "generated" image G to have similar content as the input image C. Suppose you have chosen some layer's activations to represent the content of an image. In practice, you'll get the most visually pleasing results if you choose a layer in the middle of the network--neither too shallow nor too deep. (After you have finished this exercise, feel free to come back and experiment with using different layers, to see how the results vary.)
-# 
-# So, suppose you have picked one particular hidden layer to use. Now, set the image C as the input to the pretrained VGG network, and run forward propagation. Let $a^{(C)}$ be the hidden layer activations in the layer you had chosen. (In lecture, we had written this as $a^{[l](C)}$, but here we'll drop the superscript $[l]$ to simplify the notation.) This will be a $n_H \times n_W \times n_C$ tensor. Repeat this process with the image G: Set G as the input, and run forward progation. Let $$a^{(G)}$$ be the corresponding hidden layer activation. We will define as the content cost function as:
-# 
-# $$J_{content}(C,G) =  \frac{1}{4 \times n_H \times n_W \times n_C}\sum _{ \text{all entries}} (a^{(C)} - a^{(G)})^2\tag{1} $$
-# 
-# Here, $n_H, n_W$ and $n_C$ are the height, width and number of channels of the hidden layer you have chosen, and appear in a normalization term in the cost. For clarity, note that $a^{(C)}$ and $a^{(G)}$ are the volumes corresponding to a hidden layer's activations. In order to compute the cost $J_{content}(C,G)$, it might also be convenient to unroll these 3D volumes into a 2D matrix, as shown below. (Technically this unrolling step isn't needed to compute $J_{content}$, but it will be good practice for when you do need to carry out a similar operation later for computing the style const $J_{style}$.)
-# 
-# <img src="images/NST_LOSS.png" style="width:800px;height:400px;">
-# 
-# **Exercise:** Compute the "content cost" using TensorFlow. 
-# 
-# **Instructions**: The 3 steps to implement this function are:
-# 1. Retrieve dimensions from a_G: 
-#     - To retrieve dimensions from a tensor X, use: `X.get_shape().as_list()`
-# 2. Unroll a_C and a_G as explained in the picture above
-#     - If you are stuck, take a look at [Hint1](https://www.tensorflow.org/versions/r1.3/api_docs/python/tf/transpose) and [Hint2](https://www.tensorflow.org/versions/r1.2/api_docs/python/tf/reshape).
-# 3. Compute the content cost:
-#     - If you are stuck, take a look at [Hint3](https://www.tensorflow.org/api_docs/python/tf/reduce_sum), [Hint4](https://www.tensorflow.org/api_docs/python/tf/square) and [Hint5](https://www.tensorflow.org/api_docs/python/tf/subtract).
-
-# In[4]:
 
 # GRADED FUNCTION: compute_content_cost
 
 def compute_content_cost(a_C, a_G):
-    """
-    Computes the content cost
     
-    Arguments:
-    a_C -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing content of the image C 
-    a_G -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing content of the image G
-    
-    Returns: 
-    J_content -- scalar that you compute using equation 1 above.
-    """
-    
-    ### START CODE HERE ###
-    # Retrieve dimensions from a_G (≈1 line)
+    # Retrieve dimensions from a_G 
     m, n_H, n_W, n_C = a_G.get_shape().as_list()
     
     # Reshape a_C and a_G (≈2 lines)
     a_C_unrolled = tf.transpose(a_C)
     a_G_unrolled = tf.transpose(a_G)
     
-    # compute the cost with tensorflow (≈1 line)
+    # compute the cost with tensorflow 
     J_content = (1/ (4* n_H * n_W * n_C)) * tf.reduce_sum(tf.pow((a_G_unrolled - a_C_unrolled), 2))
-    ### END CODE HERE ###
     
     return J_content
 
-
-# In[5]:
 
 tf.reset_default_graph()
 
